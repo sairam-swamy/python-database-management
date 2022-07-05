9a4219b8105b77a2e59013d389a8b2e2f3f35de4
Swapnil <46862720+Swapnil0115@users.noreply.github.com>
2022-07-05 14:59:48 +0530

    Update PYTHON.py

--- PYTHON.py
+++ PYTHON.py
@@ -4,11 +4,10 @@ class Student:
         last_name = input('Enter lasy name:')
         middle_name = input('Enter middle name:')
         course_id = int(input('Enter course id:'))
-        feeId = int(input('Enter fees ID:'))
-        user_user_ID = int(input('Enter user_user_ID:'))
-        fee_packages_fee_id = int(input('Enter fee_packages_fee_id:'))
-        Courses_course_id = int(input('Enter Courses_course_id:'))
-        admission_date = input('Enter admission date:')
+        feeid = int(input('Enter fees ID:'))
+        contact_no = input('Enter contact number:')
+        mycursor.execute(f"INSERT INTO student ,first_name, last_name, middle_name, feeid, courseid, contact_no) VALUES ('{first_name}','{last_name}', '{middle_name}','{feeid}','{course_id}', '{contact_no}');")
+        mydb.commit()
     def delete(self):
         pass
     def update(self):

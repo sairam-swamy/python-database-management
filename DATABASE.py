4747d4c62c619215d8bd387ad926006d4043032f
Nitanshu Bajpai <nitanshubajpai@gmail.com>
2022-07-05 13:53:23 +0530

    requirement, env and student table creation added

--- DATABASE.py
+++ DATABASE.py
@@ -1,1 +1,22 @@
+#pip install mysql-connector-python
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+mydb = mysql.connector.connect(
+  host=os.getenv("host"),
+  user=os.getenv("user"),
+  password=os.getenv("password"),
+  database=os.getenv("database"),
+)
+mycursor = mydb.cursor()
+# create database
+# mycursor.execute('create database student')
+# mycursor.execute('show databases')
+
+#create table
+
+#Student table create
+# mycursor.execute("drop table student")
+mycursor.execute("CREATE TABLE student(id INT AUTO_INCREMENT PRIMARY KEY,first_name varchar(20),last_name varchar(20),middle_name varchar(20),feeid varchar(10),courseid varchar(10),contact_no INT)")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # python-database-management
+
+Initialization
+
+-> python -m venv env (optional) 
+    activate env using 
+    -> env/script/activate (Windows)
+    -> env/bin/activate)    (Mac or Linux)
+
+-> Install requirement using
+    pip install -r requirements.txt
+    
+-> create a .env file with following cred
+    host="localhost"
+    user="root"
+    password="nitanshu1"
+    database="student"

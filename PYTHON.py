c9bb795fc600d29b300a4d97efcdcce3bc7e7a2c
sanketrasik <sanketrasik@gmail.com>
2022-07-05 15:59:36 +0530

    added Pay_roll class

--- PYTHON.py
+++ PYTHON.py
@@ -61,3 +61,50 @@ class Student:
 
         else:
             print(myresult)
+
+class Pay_roll:
+    mydb = None
+    mycursor = None
+    def __init__(self) -> None:
+        
+        self.mydb = get_mydb()
+        self.mycursor = self.mydb.cursor()
+
+    # ADD new pay_roll
+    def insert(self):
+        pay_roll_id = input('Enter pay_roll_id:')
+        staff_id = input('Enter staff_id:')
+        basic_salary = input('Enter basic_salary:')
+
+        self.mycursor.execute(f"INSERT INTO pay_roll (pay_roll_id, staff_id, basic_salary) VALUES ('{pay_roll_id}','{staff_id}', '{basic_salary}');")
+        self.mydb.commit()
+        
+        print(self.mycursor.rowcount, "record inserted.")
+
+    # display pay_roll 
+    def display_all(self):
+        self.mycursor.execute(f" select * from pay_roll;")
+        myresult = self.mycursor.fetchall()
+        for data in myresult:
+            print(data)
+    
+    # Search Function  
+    def search(self, id):
+        self.mycursor.execute(f" select * from pay_roll where pay_roll_id = {id}  ;")
+        myresult = self.mycursor.fetchall()
+        if len(myresult)==0:
+            print('provide a valid ID')
+        else:
+            print(myresult)
+ 
+
+    # Delete Function                
+    def delete(self, id):
+        self.mycursor.execute(f" DELETE from pay_roll where pay_roll_id = { id } ;")
+        print(self.mycursor.rowcount, "record(s) deleted")
+
+
+    # Update Function
+    def update_salary(self, id, basic_salary):
+        self.mycursor.execute(f"UPDATE pay_roll SET basic_salary = '{basic_salary}' WHERE pay_roll_id= {id} ")
+        print(self.mycursor.rowcount, "record(s) updated")

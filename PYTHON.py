57d00ab23deefcaca63dfd95e417122d53a2f7a3
Swapnil <46862720+Swapnil0115@users.noreply.github.com>
2022-07-05 14:44:51 +0530

    Created Student class

--- PYTHON.py
+++ PYTHON.py
@@ -1,1 +1,15 @@
-
+class Student:
+    def insert(self):
+        first_name = input('Enter first name:')
+        last_name = input('Enter lasy name:')
+        middle_name = input('Enter middle name:')
+        course_id = int(input('Enter course id:'))
+        feeId = int(input('Enter fees ID:'))
+        user_user_ID = int(input('Enter user_user_ID:'))
+        fee_packages_fee_id = int(input('Enter fee_packages_fee_id:'))
+        Courses_course_id = int(input('Enter Courses_course_id:'))
+        admission_date = input('Enter admission date:')
+    def delete(self):
+        pass
+    def update(self):
+        pass  
